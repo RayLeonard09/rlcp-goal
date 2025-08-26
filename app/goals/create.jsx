b5d0cd00109e9bb91333c0072fda4e67c4e50f26
RayLeonard09 <rayleonard0409@gmail.com>
2025-08-26 09:07:43 +0800

    Create and Index were changed

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#14851dff',
+    backgroundColor: '#179421ff',
     color: 'white',
     borderRadius: 8,
   }
--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#162fbbff',
+    backgroundColor: '#1329a1ff',
     color: 'white',
     borderRadius: 8,
   },
